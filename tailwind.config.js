2ef6abfaf52add4f3ff14f3bab0f3b63cda00b17
KravKo <86314507+KravKo@users.noreply.github.com>
2024-03-30 08:17:31 +0545

    chaness

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'nav-bg-color': '#1E2772',
+      },
+    },
+  },
+  plugins: [],
+}
